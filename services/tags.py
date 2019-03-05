6a73e80e3f27c916d1b94346a6ba5a49258365df
Dayna-A <dayna.anderson@csu.fullerton.edu>
2019-03-04 17:58:14 -0800

    db stuffs

--- services/tags.py
+++ services/tags.py
@@ -1,28 +1,9 @@
 import sqlite3
 import click
 from flask import g, Flask
+from .data import db
 app=Flask(__name__)
+db.init_app(app)
 
-@app.cli.command()
-def init_db():
-    with app.app_context():
-        db=get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-    click.echo("Init the db")
-
-DATABASE='#'
 """ @app.route('/')
 def my function here """
-def get_db():
-    db=getattr(g,'_database', None)
-    if db is None: 
-        db=g.database=sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None: 
-        db.close()
