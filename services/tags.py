1b645433c8ef3534cc021c6e00dda964a64e3be1
Dayna-A <dayna.anderson@csu.fullerton.edu>
2019-03-04 20:50:11 -0800

    st"

--- services/tags.py
+++ services/tags.py
@@ -2,8 +2,28 @@ import sqlite3
 import click
 from flask import g, Flask
 from .data import db
-app=Flask(__name__)
+app=Flask(__name__, instance_relative_config=True)
 db.init_app(app)
+mydb=db.get_db()
 
-""" @app.route('/')
-def my function here """
+#@app.route('/tags')
+#def my function here """
+
+def addToExisting():
+    if request.method=='POST':
+        #uncertain how to handle incoming here
+        tagname=request.TagName
+        URL=request.UrL
+
+    mydb.execute(){
+        'INSERT INTO tags(name, article)'
+        'VALUES (?,?,?)',
+        (URL, tagname)
+    }
+    mydb.commit()
+
+def addToNew():
+    if request.method=='POST':
+        #uncertain how to handle incoming here
+        tagname=request.TagName
+        URL=
