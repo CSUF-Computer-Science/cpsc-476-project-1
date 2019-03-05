bf28fe81ded0aa4f65ba00e322cddb4203f8d32b
hecmed <hector.med27@gmail.com>
2019-03-04 20:12:51 -0800

    now the database actually works

--- services/users.py
+++ services/users.py
@@ -17,6 +17,7 @@ def register_user():
         print(username, password)
         db = database.get_db()
         db.execute("INSERT INTO users(username, password, full_name) VALUES (?,?,?)", [username, password, full_name])
+        db.commit()
         database.close_db()
         return username + ' added to database!'
 
@@ -27,7 +28,7 @@ def delete_user():
         password = request.form['password']
         db = database.get_db()
         db.execute("SELECT username FROM users WHERE (?,?)", username, password)
-        database.close_db()
+        db.close_db()
 
 
 @app.route('/user/changepw', methods=['POST'])
@@ -36,7 +37,7 @@ def change_password():
         username = request.form['username']
         password = request.form['password']
         db = database.get_db()
-        database.close_db()
+        db.close_db()
 
 
 if __name__ == '__main__':
