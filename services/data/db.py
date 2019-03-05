f549ab783ed786c2a45321759a0cf94c579f3fcf
hecmed <hector.med27@gmail.com>
2019-03-04 19:46:28 -0800

    database connection should work now

--- services/data/db.py
+++ services/data/db.py
@@ -12,20 +12,19 @@ def init_app(app):
 def get_db():
     db = getattr(g, '_database', None)
     if db not in g:
-        g.database = sqlite3.connect(DATABASE)
+        db = g.database = sqlite3.connect(DATABASE)
     return db
 
-def close_db():
-    db = getattr(g, '_database', None)
+def close_db(e=None):
+    db = g.pop('db', None)
     if db is not None:
         db.close()
 
 def init_db():
     with current_app.app_context():
         db = get_db()
-    with current_app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read().decode('utf8'))
-    db.commit()
+    with current_app.open_resource('data/schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
 
 @click.command('init-db')
 @with_appcontext
