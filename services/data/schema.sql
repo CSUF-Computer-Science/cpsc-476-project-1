4436e43076cf64c7bbfb21a25c99d1e77285c246
Joshua Ferrara <scjosh2@gmail.com>
2019-03-07 13:24:54 -0800

    Added foreman def to init database. Updated schema.

--- services/data/schema.sql
+++ services/data/schema.sql
@@ -11,17 +11,21 @@ DROP TABLE IF EXISTS users;
 
 -- Table: articles
 CREATE TABLE articles (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT (128), content TEXT (512), author INT REFERENCES users (username), posted datetime default CURRENT_TIMESTAMP);
-INSERT INTO articles (title, content, author) VALUES ("My Fake Twitter BIO", "I'm a cat fan", "testuser");
+
 -- Table: comments
 CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT, author INT REFERENCES users (username), content TEXT, article INT REFERENCES articles (id), posted datetime default CURRENT_TIMESTAMP);
-INSERT INTO comments (author, article, content) VALUES ("testuser", 1, "that's super cool");
 
 -- Table: tags
 CREATE TABLE tags (name STRING (16), article INT REFERENCES articles, UNIQUE (name, article) ON CONFLICT FAIL);
-INSERT INTO tags (article, name) VALUES (1, "blessed");
-INSERT INTO tags (article, name) VALUES (1, "stayblessed");
+
 -- Table: users
 CREATE TABLE users (username STRING (32) PRIMARY KEY ON CONFLICT FAIL, password TEXT, full_name STRING (64));
--- INSERT INTO users (username, password, full_name) VALUES ("testuser", "$2b$12$DbmIZ/a5LByoJHgFItyZCeyg/DVecJAzVzmtVfFGKioGo8AqWE1XC", "Test User");
+
+-- Add some default data
+INSERT INTO users (username, password, full_name) VALUES ("testuser", "$2b$12$DbmIZ/a5LByoJHgFItyZCeyg/DVecJAzVzmtVfFGKioGo8AqWE1XC", "Test User");
+INSERT INTO articles (title, content, author) VALUES ("My Fake Twitter BIO", "I'm a cat fan", "testuser");
+INSERT INTO comments (author, article, content) VALUES ("testuser", 1, "that's super cool");
+INSERT INTO tags (article, name) VALUES (1, "blessed");
+INSERT INTO tags (article, name) VALUES (1, "stayblessed");
 
 PRAGMA foreign_keys = on;
